fc69e97c8222a875f7333fedea08374efb4f4931
Aashish <takoaashish01@gmail.com>
2023-04-24 23:33:09 -0500

    Bottom nav bar

---
+++ app/src/main/java/org/husk/foodtracer/AllListFragment.java
@@ -0,0 +1,24 @@
+package org.husk.foodtracer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AllListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AllListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_list, container, false);
+    }
+}
--- app/src/main/java/org/husk/foodtracer/MainActivity.java
+++ app/src/main/java/org/husk/foodtracer/MainActivity.java
@@ -1,14 +1,59 @@
 package org.husk.foodtracer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationBarView.OnItemSelectedListener{
+
+    private BottomNavigationView bottomNavigationView;
+
+    // Instantiating all fragments
+    AllListFragment allListFragment = new AllListFragment();
+    ExpiredListFragment expiredListFragment = new ExpiredListFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting reference for all instances declared above
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainFragment, allListFragment)
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.allList) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainFragment, allListFragment)
+                    .commit();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.expiredList) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainFragment, expiredListFragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
     }
 }
